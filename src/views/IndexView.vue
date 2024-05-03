833b5526904454e5aa15e1bef3c447d9d53bcebd
Simon_Paul <simonchasnovsky@gmail.com>
2024-05-03 14:59:20 -0300

    Finish Tailwind font setup

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -1,3 +1,4 @@
 <template>
   <span>Hello World!</span>
+  <span class="font-mono">Hello World!</span>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  mode: 'jit',
-  purge: [
-    './public/**/*.html',
-    './src/**/*.{vue,js}',
-  ],
   content: [
     "./index.html",
     "./src/**/*.{vue,js}",
@@ -12,9 +7,13 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['"Noto Sans"', ...defaultTheme.fontFamily.sans],
+        'sans': ['Noto Sans', 'sans-serif'],
+        'mono': ['Noto Sans Mono', 'monospace']
       },
     },
   },
   plugins: [],
+  corePlugins: {
+    preflight: false,
+  }
 }
